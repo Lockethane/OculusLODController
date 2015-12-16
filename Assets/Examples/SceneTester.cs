2aa2bf2df027ea8e4a8b70983cd13b5f35bdd785
Lockethane <developer@brendangm.com>
2015-12-16 02:18:40 -0500

    Set up sample scene to test POC for managing LOD's(see Nvidia Multi-Res)
    Adds LODGroupInfo for adding object level screen space LOD properties
    and new LOD mappings. Adds Scene, Assets and a test script.

---
+++ Assets/Examples/SceneTester.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneTester : MonoBehaviour {
+
+	public Transform target;
+
+	public GameObject prefab;
+	public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		UnityEngine.VR.InputTracking.Recenter ();
+
+		//Set up 10x10 grid
+		for (int row=0; row<10; ++row) {
+			for(int col=0;col<10; ++col)
+			{
+				Vector3 pos = transform.position;
+				pos.x += (-5+row);
+				pos.z += (col);
+				GameObject obj = (GameObject)Instantiate(prefab,pos,Quaternion.identity);
+				LODGroupInfo groupInfo = obj.GetComponent<LODGroupInfo>();
+				groupInfo.cam = cam;
+				groupInfo.screen_percentage_border = new Vector4(0.15f,0.15f,0.15f,0.15f);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (target == null)
+			return;
+
+		if (Input.GetKeyUp (KeyCode.UpArrow))
+			target.position += new Vector3(0,0,0.1f);
+
+		if (Input.GetKeyUp (KeyCode.DownArrow))
+			target.position += new Vector3(0,0,-0.1f);
+
+		cam.transform.Rotate(Vector3.up, 10*Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/LODGroupInfo.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Reflection;
+
+public class LODGroupInfo : MonoBehaviour {
+
+	//Camera viewing this object
+	public Camera cam;
+	//Cached Transform of LOD parent(This object)
+    Transform cached_transform;
+	//Cached LODGroup component(This object)
+	LODGroup lodGroup;
+	//Cached LODs
+    LOD[] lods;
+	//Original LOD values
+	float[] cachedTransitionsLods;
+	//Modified LOD ranges
+	public float[] customLODs;
+	//Whether or not it is in regular LOD mode
+	bool using_original_lod = true;
+    
+	//Maximum threshold in percentage of the screen from the border of the screen
+	//Order is clockwise. X:Left, Y:Top, Z:Right, W:Bottom
+	//0-1 value from border
+	public Vector4 screen_percentage_border = new Vector4(0.0f,0.0f,0.0f,0.0f);
+
+	//Min meters from camera 
+	public float min_range_buffer = 1.0f;
+
+    void Start () {
+
+		//Get the LODGroup controller to get manipulate LOD ranges
+        lodGroup = GetComponent<LODGroup>();
+
+		//Error if no LODGroup controller is on this object
+		if (lodGroup == null)
+			Debug.LogError ("No LODGroup found", this);
+
+		//Cache LOD objects
+		lods = lodGroup.GetLODs();
+
+		//Error if the number of transitions do not match
+		if (lods.Length != customLODs.Length)
+			Debug.LogError ("Inconsistent number of LOD transitions detected", this);
+
+		//Error if the LOD transitions are out of order
+		for (int testIndex=0; testIndex<customLODs.Length; ++testIndex)
+		{
+			if(testIndex > 0)
+			{
+				if(customLODs[testIndex] > customLODs[testIndex-1])
+				{
+					Debug.LogError("LOD transitions are out of order", this);
+				}
+			}
+		}
+
+		//Save the original LOD transition levels for when the object is front and center
+		cachedTransitionsLods = new float[lods.Length];
+        for(int index=0; index < customLODs.Length; ++index) 
+			cachedTransitionsLods[index] = lods[index].screenRelativeTransitionHeight;
+
+        cached_transform = transform;
+    }
+	
+	void Update () 
+	{
+        //Get the screen space of the object
+        Vector3 screenPos = cam.WorldToScreenPoint(cached_transform.position);
+
+		//Get the 0-1 percent of where the object is on the screen from the left/bottom
+		float screen_percent_positionX = screenPos.x / Screen.width;
+		float screen_percent_positionY = screenPos.y / Screen.height;
+
+		//Is the object on the borders set within the threshold
+		bool within_threshold = (screen_percent_positionX < screen_percentage_border.x || screen_percent_positionX > (1.0f - screen_percentage_border.z)) ||
+								(screen_percent_positionY < screen_percentage_border.y || screen_percent_positionY > (1.0f - screen_percentage_border.w));
+
+		//Is the object far enough away from the camera
+		bool outside_range_border = screenPos.z > min_range_buffer;
+
+		//Test whether or not to use custom transitions
+		if (outside_range_border && within_threshold)
+			SetLods (customLODs, false);
+		else
+			SetLods (cachedTransitionsLods, true);
+    }
+
+	private void SetLods(float[] lod_transitions, bool original_lod)
+	{
+		//Camera conditions haven't changed between thresholds
+		if (using_original_lod == original_lod)
+			return;
+
+		//Change the screen perecentage that the object has to be for the lod to take place
+		for(int index=0; index < lod_transitions.Length; ++index)
+		{
+			lods[index].screenRelativeTransitionHeight = lod_transitions[index];
+		}
+		//Update the objects lod manager
+		lodGroup.SetLODs(lods);
+
+		//Toggle wether or not the object is using original LOD's to shortcut calling SetLODs
+		using_original_lod = original_lod;
+	}
+}
