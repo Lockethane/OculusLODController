526d935927aadf030ac7b359b2be73930e8cfc11
Lockethane <developer@brendangm.com>
2016-03-20 22:42:34 -0400

    Updated threshold calculations
    Added circle threshold, separated threshold calculation into function.

--- Assets/Scripts/Editor/LODGroupInfoEditor.cs
+++ Assets/Scripts/Editor/LODGroupInfoEditor.cs
@@ -7,50 +7,64 @@ namespace VRLODController
     public class LevelScriptEditor : Editor
     {
         GUIContent text;
-        SerializedProperty camera;
-        SerializedProperty customLODs;
-        SerializedProperty screenBorderLeft;
-        SerializedProperty screenBorderTop;
-        SerializedProperty screenBorderRight;
-        SerializedProperty screenBorderBottom;
-
+        
         void OnEnable()
         {
             text = new GUIContent("");
-            camera = serializedObject.FindProperty("cam");
-            customLODs = serializedObject.FindProperty("customLODs");
-            screenBorderLeft = serializedObject.FindProperty("screenPercentageBorder.x");
-            screenBorderTop = serializedObject.FindProperty("screenPercentageBorder.y");
-            screenBorderRight = serializedObject.FindProperty("screenPercentageBorder.z");
-            screenBorderBottom = serializedObject.FindProperty("screenPercentageBorder.w");
-
-
         }
 
         public override void OnInspectorGUI()
         {
+            SerializedProperty customLODs = serializedObject.FindProperty("customLODs");
             text.text = "CUstom LOD distances";
             EditorGUI.BeginChangeCheck();
             EditorGUILayout.PropertyField(customLODs, text, true);
             if (EditorGUI.EndChangeCheck())
                 serializedObject.ApplyModifiedProperties();
-       
-            GUIStyle style = new GUIStyle(GUI.skin.GetStyle("Label"));
-            style.alignment = TextAnchor.MiddleCenter;
-            EditorGUILayout.LabelField("Screen border percentage", style);
 
+            SerializedProperty thresholdType = serializedObject.FindProperty("thresholdType");
+            text.text = "Threshold function";
             EditorGUI.BeginChangeCheck();
-            text.text = "Left border";
-            EditorGUILayout.Slider(screenBorderLeft, 0, 0.5f, text);
-            text.text = "Top border";
-            EditorGUILayout.Slider(screenBorderTop, 0.5f, 1, text);
-            text.text = "Right border";
-            EditorGUILayout.Slider(screenBorderRight, 0.5f, 1, text);
-            text.text = "Bottom border";
-            EditorGUILayout.Slider(screenBorderBottom, 0, 0.5f, text);
+            EditorGUILayout.PropertyField(thresholdType, text, true);
             if (EditorGUI.EndChangeCheck())
                 serializedObject.ApplyModifiedProperties();
 
+            if(thresholdType.intValue == (int)LODGroupInfo.ThresholdType.Square)
+            { 
+                GUIStyle style = new GUIStyle(GUI.skin.GetStyle("Label"));
+                style.alignment = TextAnchor.MiddleCenter;
+                EditorGUILayout.LabelField("Screen border percentage", style);
+
+                EditorGUI.BeginChangeCheck();
+
+                text.text = "Left border";
+                SerializedProperty screenBorderLeft = serializedObject.FindProperty("screenPercentageBorder.x");
+                EditorGUILayout.Slider(screenBorderLeft, 0, 0.5f, text);
+
+                text.text = "Top border";
+                SerializedProperty screenBorderTop = serializedObject.FindProperty("screenPercentageBorder.y");
+                EditorGUILayout.Slider(screenBorderTop, 0.5f, 1, text);
+
+                text.text = "Right border";
+                SerializedProperty screenBorderRight = serializedObject.FindProperty("screenPercentageBorder.z");
+                EditorGUILayout.Slider(screenBorderRight, 0.5f, 1, text);
+
+                text.text = "Bottom border";
+                SerializedProperty screenBorderBottom = serializedObject.FindProperty("screenPercentageBorder.w");
+                EditorGUILayout.Slider(screenBorderBottom, 0, 0.5f, text);
+
+                if (EditorGUI.EndChangeCheck())
+                    serializedObject.ApplyModifiedProperties();
+            }
+            else if(thresholdType.intValue == (int)LODGroupInfo.ThresholdType.Circle)
+            {
+                text.text = "Distance from center";
+                SerializedProperty distanceThreshold = serializedObject.FindProperty("distanceFromCenter");
+                EditorGUI.BeginChangeCheck();
+                EditorGUILayout.PropertyField(distanceThreshold, text, true);
+                if (EditorGUI.EndChangeCheck())
+                    serializedObject.ApplyModifiedProperties();
+            }
         }
     }
 }
--- Assets/Scripts/LODGroupInfo.cs
+++ Assets/Scripts/LODGroupInfo.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Reflection;
+using System;
 
 namespace VRLODController
 { 
 
     public class LODGroupInfo : MonoBehaviour {
+
+        public enum ThresholdType
+        {
+            Square = 0,
+            Circle = 1
+        }
         
 	    //Cached Transform of LOD parent(This object)
         Transform cachedTransform;
@@ -24,13 +31,21 @@ namespace VRLODController
             get { return usingOriginalLod; }
         }
 
+        public ThresholdType thresholdType = ThresholdType.Square;
+
         //Maximum threshold in percentage of the screen from the border of the screen
         //Order is clockwise. X:Left, Y:Top, Z:Right, W:Bottom
         //0-.5 value from left/bottom border to center, .5-1 from center to top/right border
         public Vector4 screenPercentageBorder = new Vector4(0.0f,0.0f,0.0f,0.0f);
 
+        //Used to calculate if object is inside circular threshold
+        [Range(0.0f,1.0f)]
+        public float distanceFromCenter = 1.0f;
+
 	    //Minimun distance in meters from camera before applying modifiers 
 	    public float minActivationDistance = 1.0f;
+
+        private Func<Vector3, bool> thresholdDelegate;
 
         public void Start()
         {
@@ -67,21 +82,41 @@ namespace VRLODController
 			    cachedTransitionsLods[index] = lods[index].screenRelativeTransitionHeight;
 
             cachedTransform = transform;
+
+            switch(thresholdType)
+            {
+                case ThresholdType.Square:
+                    thresholdDelegate = OutsideThresholdRect;
+                    break;
+                case ThresholdType.Circle:
+                    thresholdDelegate = OutsideThresholdCircle;
+                    break;
+                default:
+                    Debug.LogError("No function defined");
+                    break;
+            }
         }
         
         public void LODUpdate(Camera cam) 
 	    {
             //Get the screen space of the object
             Vector3 screenPos = cam.WorldToScreenPoint(cachedTransform.position);
-		
-            //Get the 0-1 percent of where the object is on the screen from the left/bottom
-            //Clamp to 0-1 since if the LOD is outside the screen, the system shouldn't care
-		    float screenPercentPositionX = Mathf.Clamp01( screenPos.x / Screen.width);
-		    float screenPercentPositionY = Mathf.Clamp01( screenPos.y / Screen.height);
 
-		    //Is the object on the borders set by the threshold
-		    bool outsideThreshold = (screenPercentPositionX < screenPercentageBorder.x || screenPercentPositionX > (screenPercentageBorder.z)) ||
-								    (screenPercentPositionY > screenPercentageBorder.y || screenPercentPositionY < (screenPercentageBorder.w));
+            //Is the object on the borders set by the threshold
+            bool outsideThreshold = false;
+
+            switch (thresholdType)
+            {
+                case ThresholdType.Square:
+                    outsideThreshold = OutsideThresholdRect(screenPos);
+                    break;
+                case ThresholdType.Circle:
+                    outsideThreshold = OutsideThresholdCircle(screenPos);
+                    break;
+                default:
+                    Debug.LogError("No function defined");
+                    break;
+            }
 
 		    //Is the object far enough away from the camera
 		    bool outside_range_border = Mathf.Abs(screenPos.z) > minActivationDistance;
@@ -115,5 +150,39 @@ namespace VRLODController
 		    //Toggle wether or not the object is using original LOD's to shortcut calling SetLODs
 		    usingOriginalLod = originaLOD;
 	    }
+
+        /// Defined Threshold functions
+        private bool OutsideThresholdRect(Vector3 screenPos)
+        {
+            //Get the 0-1 percent of where the object is on the screen from the left/bottom
+            //Clamp to 0-1 since if the LOD is outside the screen, the system shouldn't care
+            float screenPercentPositionX = Mathf.Clamp01(screenPos.x / Screen.width);
+            float screenPercentPositionY = Mathf.Clamp01(screenPos.y / Screen.height);
+
+            //Is the object on the borders set by the threshold
+            bool outsideThreshold = (screenPercentPositionX < screenPercentageBorder.x || screenPercentPositionX > (screenPercentageBorder.z)) ||
+                                    (screenPercentPositionY > screenPercentageBorder.y || screenPercentPositionY < (screenPercentageBorder.w));
+
+            return outsideThreshold;
+        }
+
+        private bool OutsideThresholdCircle(Vector3 screenPos)
+        {
+            Vector2 screenCenter = new Vector2((Screen.width/2)/(float)Screen.width, (Screen.height/2) / (float)Screen.height);
+
+            //Get the 0-1 percent of where the object is on the screen from the left/bottom
+            //Clamp to 0-1 since if the LOD is outside the screen, the system shouldn't care
+            float screenPercentPositionX = Mathf.Clamp01(screenPos.x / Screen.width);
+            float screenPercentPositionY = Mathf.Clamp01(screenPos.y / Screen.height);
+
+            float xDistance = screenPercentPositionX - screenCenter.x;
+            float yDistance = screenPercentPositionY - screenCenter.y;
+            float distanceSquared = xDistance * xDistance + yDistance * yDistance;
+            
+            //Is the object on the borders set by the threshold
+            bool outsideThreshold = distanceSquared > (distanceFromCenter* distanceFromCenter);
+
+            return outsideThreshold;
+        }
     }
 }
